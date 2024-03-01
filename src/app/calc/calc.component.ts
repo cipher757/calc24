91d011766c5961477b83ff7d156d1c3cd392a09d
João Otávio <jofs2006@gmail.com>
2024-03-01 14:24:07 -0300

    more visual changes

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -8,30 +8,24 @@ import { Component } from '@angular/core';
 export class CalcComponent {
    num1: number = 0;
    num2: number = 0;
-   num3: number = 0;
-   num4: number = 0;
-   num5: number = 0;
-   num6: number = 0;
-   num7: number = 0;
-   num8: number = 0;
    soma: number = 0;
    subtracao: number = 0;
    multiplicacao: number = 0;
    divisao: number = 0;
 
    calc(){
-     this.soma = this.num1 + this.num2; 
+     this.soma = this.num1 + this.num2;
    }
 
    calc2(){
-    this.subtracao = this.num3 - this.num4;
+    this.subtracao = this.num1 - this.num2;
    }
 
    calc3(){
-    this.multiplicacao = this.num5 * this.num6;
+    this.multiplicacao = this.num1 * this.num2;
    }
 
    calc4(){
-    this.divisao = this.num7 / this.num8;
+    this.divisao = this.num1 / this.num2;
    }
 }
